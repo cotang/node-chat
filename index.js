0ae2fb58e6e5cc3d5fc2a7a7b680f25e27386539
Konstantin Bogdanov <konstantin.bogdanov@dataart.com>
2019-06-27 18:58:03 +0300

    auth

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
-var bodyParser = require('body-parser');
-const port = 8080;
+const bodyParser = require('body-parser');
+const port = 8100;
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/testNode');
+const db = mongoose.connection;
 
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,7 +36,7 @@ app.use(express.static(__dirname + '/public'))
 
 // Routing
 // include routes
-var routes = require('./routes/router');
+const routes = require('./routes/router');
 app.use('/', routes);
 
 
